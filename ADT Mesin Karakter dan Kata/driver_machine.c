ab0326a2fa59b63b9fa1658d7dddfa35ee8c2e43
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-10-28 04:01:01 +0700

    add driver

---
+++ ADT Mesin Karakter dan Kata/driver_machine.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include "boolean.h"
+#include "charmachine.c"
+#include "charmachine.h"
+#include "wordmachine.h"
+#include "wordmachine.c"
+
+boolean endWord;
+Word currentWord;
+
+int main(){
+    // check is kata equal
+    Word kata;
+    kata.length = 3;
+    kata.contents[0] = 'a';
+    kata.contents[1] = 'b';
+    kata.contents[2] = 'c';
+    startWord_command();
+    boolean found = isWordEqual(currentWord, kata);
+    if (found){
+        printf("yoi\n");
+    }
+
+    // check print word
+    printWord(currentWord);
+    printf("\n");
+
+    // word input (mark masih . )
+    int i;
+    while (!endWord){
+        for (i=0; i < currentWord.length; i++){ 
+            printf("%c", currentWord.contents[i]); 
+        } 
+        if (currentChar == MARK){
+            printf("%c\n", MARK);
+        }
+        else {
+            printf("%c", BLANK);
+        }
+        advWord();
+    }
+
+    // file input (mark masih . )
+    startWord_file("tes_config.txt");
+    while (!endWord){
+        for (i = 0; i < currentWord.length; i++){
+            printf("%c", currentWord.contents[i]);
+        }
+        if (currentChar == MARK){
+            printf("%c\n", MARK);
+        }
+        else{
+            printf("%c", BLANK);
+        }
+        advWord();
+    }
+    printf("YOI \n NEXT \n \n");
+    printf("Udah sampe sini\n");
+}
