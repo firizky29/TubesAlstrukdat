a8be99a01566b2dee01cef9a6b5a97afaa1f3d85
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-10-28 03:59:56 +0700

    add ADT mesin karakter

---
+++ ADT Mesin Karakter dan Kata/charmachine.h
@@ -0,0 +1,35 @@
+#ifndef CHAR_ENGINE_H
+#define CHAR_ENGINE_H
+
+#include "boolean.h"
+#include <string.h>
+
+#define MARK '.'
+/* Char Engine State */
+extern char currentChar;
+extern boolean eot;
+
+void start_file(char *filename);
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
+
+void start_command();
+/* Mesin siap dioperasikan. Pita disiapkan untuk dibaca.
+   Karakter pertama yang ada pada pita posisinya adalah pada jendela.
+   I.S. : sembarang
+   F.S. : currentChar adalah karakter pertama pada pita
+          Jika currentChar != MARK maka EOP akan padam (false)
+          Jika currentChar = MARK maka EOP akan menyala (true) */
+
+void adv();
+/* Pita dimajukan satu karakter. 
+   I.S. : Karakter pada jendela = currentChar, currentChar != MARK
+   F.S. : currentChar adalah karakter berikutnya dari currentChar yang lama, 
+          currentChar mungkin = MARK
+          Jika  currentChar = MARK maka EOP akan menyala (true) */
+
+#endif
