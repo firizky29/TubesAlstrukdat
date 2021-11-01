da8e8843244ee87f4e7198757032a0e1ec1cb2ac
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-11-02 04:38:47 +0700

    adjust config info to matrix/point/listdin

--- src/lib/tools/mesin-kata/wordmachine.c
+++ src/lib/tools/mesin-kata/wordmachine.c
@@ -1,136 +1,98 @@
 #include <stdio.h>
-#include "boolean.h"
-#include "wordmachine.h"
-#include "../file-reader/filecharmachine.h"
+#include <stdlib.h>
+
+#include "../../data-structure/list-dinamis/listdin.h"
+#include "../../data-structure/matriks/matrix.h"
+#include "../../data-structure/point/point.h"
+#include "../mesin-karakter/charmachine.h"
+#include "../../boolean.h"
 
 /* Word Engine State */
 boolean endWord;
 Word currentWord;
 
-void ignoreBlank_file(){
-/* Mengabaikan satu atau beberapa BLANK
-   I.S. : currentChar sembarang 
-   F.S. : currentChar != BLANK atau currentChar = EOF */
-	while (currentCharFile == BLANK){
-		advFile();
-	}
-}
-
-void ignoreBlank_command(){
-/* Mengabaikan satu atau beberapa BLANK
-   I.S. : currentChar sembarang 
-   F.S. : currentChar != BLANK atau currentChar = MARK */
+/* PRIMITIVE FUNCTIONS (File & CMD) */
+void ignoreBlank(){
+/* self describing function */
 	while (currentChar == BLANK){
 		adv();
 	}
 }
 
-void startWord_file(char *filename){
-/* 
-   I.S. : currentChar sembarang
-   F.S. : endWord = true, dan currentChar = EOF; 
-          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-	startFile(filename);
-	ignoreBlank_file();
-	if (currentCharFile == MARKFILE){
-		endWord = true;
-	}
-	else{
-		copyWord_file();
-		endWord = false;
-	}
-}
-
-void startWord_command(){
-/* 
-   I.S. : currentChar sembarang
-   F.S. : endWord = true, dan currentChar = MARK; 
-          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-	start();
-	ignoreBlank_command();
-	if (currentChar == MARK){
-		endWord = true;
-	}
-	else{
-		copyWord_command();
-		endWord = false;
-	}
-}
-
-void advWord_command(){
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
-          Jika currentChar = MARK, endWord = true.		  
-   Proses : Akuisisi kata menggunakan procedure copyWord */
-	ignoreBlank_command();
-	if (currentChar == MARK){
-		endWord = true;
-	}
-	else{
-		copyWord_command();
-		ignoreBlank_command();
-	}
-}
-
-void advWord_file(){
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin EOF
-          Jika currentChar = EOF, endWord = true.		  
-   Proses : Akuisisi kata menggunakan procedure copyWord */
-	ignoreBlank_file();
-	if (currentCharFile == MARKFILE){
-		endWord = true;
-	}
-	else{
-		copyWord_file();
-		ignoreBlank_file();
-	}
-}
-
-
-void copyWord_command(){
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : currentChar adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi; 
-          currentChar = BLANK atau currentChar = MARK; 
-          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
+void copyWord_file(){
+/* copy word buat input dari file */
 	int i = 0;
-	while (currentChar != MARK && currentChar != BLANK){
+	while (currentChar != BLANK && !endFile){
 		currentWord.contents[i] = currentChar;
 		i++;
 		adv();
 	}
-	if (i > CAPACITY){
-		currentWord.length = CAPACITY;
+	if (i > MAXWORD){
+		currentWord.length = MAXWORD;
 	}
 	else{
 		currentWord.length = i;
 	}
 }
 
-void copyWord_file(){
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : currentChar adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi; 
-          currentChar = BLANK atau currentChar = EOF; 
-          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
+void copyWord_command(){
+/* copy word buat input dari command */
 	int i = 0;
-	while (currentCharFile != BLANK && currentCharFile != MARKFILE){
-		currentWord.contents[i] = currentCharFile;
+	while (currentChar != MARK && currentChar != BLANK){
+		currentWord.contents[i] = currentChar;
 		i++;
-		advFile();
+		adv_command();
 	}
-	if (i > CAPACITY){
-		currentWord.length = CAPACITY;
+	if (i > MAXWORD){
+		currentWord.length = MAXWORD;
 	}
 	else{
 		currentWord.length = i;
+	}
+}
+
+void startWord_file(char *filename){
+/* start word buat input dari file, marknya EOF */
+	endFile = false;
+	start_file(filename);
+	ignoreBlank();
+	copyWord_file();
+}
+
+void startWord_command(){
+/* start word buat input dari command, marknya newline */
+	start_command();
+	ignoreBlank();
+	if (currentChar == MARK){
+		endWord = true;
+	}
+	else{
+		copyWord_command();
+		endWord = false;
+	}
+}
+
+void advWord_file(){
+/* advance ke word berikutnya buat input file */
+	if (currentWord.length != 0){
+		memset (currentWord.contents, 0, strlen(currentWord.contents));
+	}
+	ignoreBlank();
+	copyWord_file();
+}
+
+void advWord_command(){
+/* advance ke word berikutnya buat input file */
+	if (currentWord.length != 0){
+		memset (currentWord.contents, 0, strlen(currentWord.contents));
+	}
+	ignoreBlank();
+	if (currentChar == MARK){
+		endWord = true;
+	}
+	else{
+		copyWord_file();
+		ignoreBlank();
 	}
 }
 
@@ -150,11 +112,174 @@ boolean isWordEqual(Word kata1, Word kata2){
 	}
 }
 
+/* READ INPUT & PRINT OUTPUT (File & CMD) */
+Word inputWord(){
+/* menerima input dan direturnd dalam bentuk word */
+	startWord_command();
+	Word input = currentWord;
+	return input;
+}
+
+void readFile(Word filename){
+/* membaca file dengan nama filename */
+	startWord_file(filename.contents);
+}
+
+void printWordFile(Word kata){ 
+/* print kata dari suatu file */
+	printWord(kata);
+	printf("%c", BLANK);
+	advWord_file();
+}
+
 void printWord(Word kata){
 /* print kata */
 	int i;
-	for (i = 0; i < kata.length; i++){
-		printf("%c", kata.contents[i]);
+	for (i=0; i < kata.length; i++){ 
+		printf("%c", kata.contents[i]); 
 	}
 }
 
+/* CONVERT WORD */
+int wtoi(Word kata){
+/* convert word to integer */
+	int x = atoi(kata.contents);
+    advWord_file();
+	return x;
+}
+
+Matrix getMap(){
+/* get n x m from config and return n x m matrix */
+	int n, m;
+	n = wtoi(currentWord);
+	m = wtoi(currentWord);
+	Matrix map;
+	CreateMatrix(n, m, &map);
+	return map;
+}
+
+POINT getPoint(){
+/* get and return point from config */
+	int x, y;
+	x = wtoi(currentWord);
+	y = wtoi(currentWord);
+	POINT hq = MakePOINT(x, y);
+	return hq;
+}
+
+Matrix getAdjacency(int n){
+/* get and return adjacency matrix from config */
+	int temp, i, j;
+	Matrix adjacency;
+	CreateMatrix(n, n, &adjacency);
+	for (i = 0; i < n; i++){
+		for (j = 0; j < n; j++){
+			temp = wtoi(currentWord);
+			ELMT(adjacency, i, j) = temp;
+		}
+	}
+	return adjacency;
+}
+
+ListDin getLoc(POINT pointhq){
+/* get char and coord of loc and return list dinamis */
+	int i;
+	ListDin loc;
+	int num = wtoi(currentWord);
+	CreateListDin(&loc, (num+1));
+	CHARLOC(loc, 0) = '8';
+	COORLOC(loc, 0) = pointhq;
+	for (i = 1; i < (num + 1); i++){
+		CHARLOC(loc, i) = currentWord.contents[0];
+		advWord_file();
+		COORLOC(loc, i) = getPoint();
+	}
+	NEFF(loc) = loc.capacity;
+	return loc;
+}
+
+
+/* CHECK COMMAND VALIDITY */
+boolean isNewGame(Word kata){
+/* return true if kata is NEW_GAME and false if not */
+	Word cmd_newgame = {"NEW_GAME", 8};
+	return isWordEqual(kata, cmd_newgame);
+}
+
+boolean isExit(Word kata){
+/* return true if kata is EXIT and false if not */
+	Word cmd_exit = {"EXIT", 4};
+	return isWordEqual(kata, cmd_exit);
+}
+
+boolean isLoadGame(Word kata){
+/* return true if kata is LOAD_GAME and false if not */
+	Word cmd_loadgame = {"LOAD_GAME", 9};
+	return isWordEqual(kata, cmd_loadgame);
+}
+
+boolean isMove(Word kata){
+/* return true if kata is MOVE and false if not */
+	Word cmd_move = {"MOVE", 4};
+	return isWordEqual(kata, cmd_move);
+}
+
+boolean isPickUp(Word kata){
+/* return true if kata is PICK_UP and false if not */
+	Word cmd_pickup = {"PICK_UP", 7};
+	return isWordEqual(kata, cmd_pickup);
+}
+
+boolean isDropOff(Word kata){
+/* return true if kata is DROP_OFF and false if not */
+	Word cmd_dropoff = {"DROP_OFF", 8};
+	return isWordEqual(kata, cmd_dropoff);
+}
+
+boolean isMap(Word kata){
+/* return true if kata is MAP and false if not */
+	Word cmd_map = {"MAP", 3};
+	return isWordEqual(kata, cmd_map);
+}
+
+boolean isToDo(Word kata){
+/* return true if kata is TO_DO and false if not */
+	Word cmd_todo = {"TO_DO", 5};
+	return isWordEqual(kata, cmd_todo);
+}
+
+boolean isInProgress(Word kata){
+/* return true if kata is IN_PROGRESS and false if not */
+	Word cmd_inprogress = {"IN_PROGRESS", 11};
+	return isWordEqual(kata, cmd_inprogress);
+}
+
+boolean isBuy(Word kata){
+/* return true if kata is BUY and false if not */
+	Word cmd_buy = {"BUY", 3};
+	return isWordEqual(kata, cmd_buy);
+}
+
+boolean isInventory(Word kata){
+/* return true if kata is INVENTORY and false if not */
+	Word cmd_inventory = {"INVENTORY", 9};
+	return isWordEqual(kata, cmd_inventory);
+}
+
+boolean isHelp(Word kata){
+/* return true if kata is HELP and false if not */
+	Word cmd_help = {"HELP", 4};
+	return isWordEqual(kata, cmd_help);
+}
+
+boolean isSaveGame(Word kata){
+/* return true if kata is SAVE_GAME and false if not */
+	Word cmd_savegame = {"SAVE_GAME", 9};
+	return isWordEqual(kata, cmd_savegame);
+}
+
+boolean isReturn(Word kata){
+/* return true if kata is RETURN and false if not */
+	Word cmd_return = {"RETURN", 6};
+	return isWordEqual(kata, cmd_return);
+}
--- src/lib/tools/mesin-kata/wordmachine.h
+++ src/lib/tools/mesin-kata/wordmachine.h
@@ -1,15 +1,17 @@
 #ifndef WORD_ENGINE_H
 #define WORD_ENGINE_H
 
-#include "boolean.h"
+#include "../../data-structure/list-dinamis/listdin.h"
+#include "../../data-structure/matriks/matrix.h"
+#include "../../data-structure/point/point.h"
 #include "../mesin-karakter/charmachine.h"
-#include "../file-reader/filecharmachine.h"
+#include "../../boolean.h"
 
-#define CAPACITY 50
+#define MAXWORD 50
 #define BLANK ' '
 
 typedef struct {
-   char contents[CAPACITY]; /* container penyimpan kata, indeks yang dipakai [0..CAPACITY-1] */
+   char contents[MAXWORD]; /* container penyimpan kata, indeks yang dipakai [0..CAPACITY-1] */
    int length;
 } Word;
 
@@ -17,42 +19,18 @@ typedef struct {
 extern boolean endWord;
 extern Word currentWord;
 
-void ignoreBlank_command();
+/* PRIMITIVE FUNCTIONS: From File & Command */
+void ignoreBlank();
 /* Mengabaikan satu atau beberapa BLANK
    I.S. : currentChar sembarang 
    F.S. : currentChar ? BLANK atau currentChar = MARK */
-   
-void ignoreBlank_file();
-/* Mengabaikan satu atau beberapa BLANK
-   I.S. : currentChar sembarang 
-   F.S. : currentChar ? BLANK atau currentChar = EOF */
-
-void startWord_file(char *filename);
-/* I.S. : currentChar sembarang 
-   F.S. : endWord = true, dan currentChar = MARK; 
-          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-
-void startWord_command();
-/* I.S. : currentChar sembarang
-   F.S. : endWord = true, dan currentChar = MARK; 
-          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
-          currentChar karakter pertama sesudah karakter terakhir kata */
-
-void advWord_command();
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
-          Jika currentChar = MARK, endWord = true.		  
-   Proses : Akuisisi kata menggunakan procedure copyWord */
-
-void advWord_file();
-/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
-   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
-          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
-          Jika currentChar = EOF, endWord = true.		  
-   Proses : Akuisisi kata menggunakan procedure copyWord */
-
+void copyWord_file();
+/* Mengakuisisi kata, menyimpan dalam currentWord
+   I.S. : currentChar adalah karakter pertama dari kata
+   F.S. : currentWord berisi kata yang sudah diakuisisi; 
+          currentChar = BLANK atau currentChar = MARK; 
+          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
+          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
 void copyWord_command();
 /* Mengakuisisi kata, menyimpan dalam currentWord
    I.S. : currentChar adalah karakter pertama dari kata
@@ -60,19 +38,84 @@ void copyWord_command();
           currentChar = BLANK atau currentChar = MARK; 
           currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
           Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
-
-void copyWord_file();
-/* Mengakuisisi kata, menyimpan dalam currentWord
-   I.S. : currentChar adalah karakter pertama dari kata
-   F.S. : currentWord berisi kata yang sudah diakuisisi; 
-          currentChar = BLANK atau currentChar = EOF; 
-          currentChar adalah karakter sesudah karakter terakhir yang diakuisisi.
-          Jika panjang kata melebihi CAPACITY, maka sisa kata terpotong */
-
+void startWord_file(char *filename);
+/* I.S. : currentChar sembarang 
+   F.S. : endWord = true, dan currentChar = MARK; 
+          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
+          currentChar karakter pertama sesudah karakter terakhir kata */
+void startWord_command();
+/* I.S. : currentChar sembarang
+   F.S. : endWord = true, dan currentChar = MARK; 
+          atau endWord = false, currentWord adalah kata yang sudah diakuisisi,
+          currentChar karakter pertama sesudah karakter terakhir kata */
+void advWord();
+/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
+   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
+          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika currentChar = MARK, endWord = true.		  
+   Proses : Akuisisi kata menggunakan procedure copyWord */
+void advWord_command();
+/* I.S. : currentChar adalah karakter pertama kata yang akan diakuisisi 
+   F.S. : currentWord adalah kata terakhir yang sudah diakuisisi, 
+          currentChar adalah karakter pertama dari kata berikutnya, mungkin MARK
+          Jika currentChar = MARK, endWord = true.		  
+   Proses : Akuisisi kata menggunakan procedure copyWord */
 boolean isWordEqual(Word kata1, Word kata2);
-/* bandingin dua kata */
+/* membandingkan dua kata dan return true kalo sama, return false kalo beda */
 
+
+/* READ INPUT & PRINT OUTPUT (File & CMD) */
+Word inputWord();
+/* menerima input dari command line dan dijadikan output berupa Word */
+void readFile(Word filename);
+/* membaca file dengan nama filename */
+void printWordFile(Word kata);
+/* print kata dari suatu file */
 void printWord(Word kata);
 /* print kata */
+
+/* CONVERT WORD */
+int wtoi(Word kata);
+/* convert word to integer */
+
+/* GET INFO FROM CONFIG FILE */
+Matrix getMap();
+/* get n x m from config and return n x m matrix */
+POINT getPoint();
+/* get and return point from config */
+Matrix getAdjacency(int n);
+/* get and return adjacency matrix from config */
+ListDin getLoc(POINT pointhq);
+/* get char and coord of loc and return list dinamis */
+
+/* CHECK COMMAND VALIDITY */
+boolean isNewGame(Word kata);
+/* return true if kata is NEW_GAME and false if not */
+boolean isExit(Word kata);
+/* return true if kata is EXIT and false if not */
+boolean isLoadGame(Word kata);
+/* return true if kata is LOAD_GAME and false if not */
+boolean isMove(Word kata);
+/* return true if kata is MOVE and false if not */
+boolean isPickUp(Word kata);
+/* return true if kata is PICK_UP and false if not */
+boolean isDropOff(Word kata);
+/* return true if kata is DROP_OFF and false if not */
+boolean isMap(Word kata);
+/* return true if kata is MAP and false if not */
+boolean isToDo(Word kata);
+/* return true if kata is TO_DO and false if not */
+boolean isInProgress(Word kata);
+/* return true if kata is IN_PROGRESS and false if not */
+boolean isBuy(Word kata);
+/* return true if kata is BUY and false if not */
+boolean isInventory(Word kata);
+/* return true if kata is INVENTORY and false if not */
+boolean isHelp(Word kata);
+/* return true if kata is HELP and false if not */
+boolean isSaveGame(Word kata);
+/* return true if kata is SAVE_GAME and false if not */
+boolean isReturn(Word kata);
+/* return true if kata is RETURN and false if not */
 
 #endif
