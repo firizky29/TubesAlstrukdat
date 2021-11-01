c7de24e75480d20feb6ca72f3b8469a3954a330a
Sarah Azka Arief <sarah_azka@yahoo.com>
2021-11-02 04:40:08 +0700

    add main menu + get config info

--- src/lib/tools/mesin-kata/driver_machine.c
+++ src/lib/tools/mesin-kata/driver_machine.c
@@ -1,55 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#include "../../data-structure/list-dinamis/listdin.h"
+#include "../../data-structure/matriks/matrix.h"
+#include "../../data-structure/point/point.h"
+#include "../mesin-karakter/charmachine.h"
+#include "../../boolean.h"
 #include "wordmachine.h"
 
-boolean endWord;
-Word currentWord;
+/* SIMULASI MAIN MENU + BACA CONFIG
+dari tahap main menu sampe get info dari config 
+baru sampe get map matrix, matrix adjacency, sama point lokasi2 */
 
 int main(){
-    // check is kata equal
-    Word kata;
-    kata.length = 3;
-    kata.contents[0] = 'a';
-    kata.contents[1] = 'b';
-    kata.contents[2] = 'c';
-    startWord();
-    boolean found = isWordEqual(currentWord, kata);
-    if (found){
-        printf("yoi\n");
-    }
-
-    // check print word
-    printWord(currentWord);
-    printf("\n");
-
-    // word input (mark masih . )
-    int i;
-    while (!endWord){
-        for (i=0; i < currentWord.length; i++){ 
-            printf("%c", currentWord.contents[i]); 
-        } 
-        if (currentChar == MARK){
-            printf("%c\n", MARK);
+    // main menu
+	printf("Type one of the following:\n1. NEW_GAME\n2. EXIT\nEnter your choice: ");
+	Word choice = inputWord();
+	while (!isNewGame(choice) && !isExit(choice)){
+		printf("Stop joking around, that's an invalid command. Try again: ");
+		choice = inputWord();
+	}
+    // NEW_GAME (start new game)
+	if (isNewGame(choice)){
+		printf("Enter configuration file level ([filename].txt): ");
+		Word filename = inputWord();
+		while (!fopen(filename.contents, "r")){
+			printf("File not found, enter filename again: ");
+			filename = inputWord();
+		}
+		printf("Opening ");
+		printWord(filename);
+		printf("\n");
+        
+        // reading file config + get info
+		readFile(filename);
+        Matrix map = getMap();
+        POINT hq = getPoint();
+        ListDin loc = getLoc(hq);
+        Matrix adj = getAdjacency(loc.capacity);
+        // display info
+        printf("\nHQ berada pada (x,y): (%f, %f)\n\n", hq.X, hq.Y);
+        printf("Map: %d x %d\n", map.rowEff, map.colEff);
+        displayMatrix(map);
+        printf("\n\n");
+        displayMatrix(adj);
+        printf("\n\n");
+        displayList(loc);
+        printf("\n\n");
+        // sisanya di print dulu aja karena nunggu ADTnya ada dulu
+        while(!endFile){
+            printWordFile(currentWord);
         }
-        else {
-            printf("%c", BLANK);
-        }
-        advWord();
-    }
-
-    // file input (mark masih . )
-    startWord_file("tes_config.txt");
-    while (!endWord){
-        for (i = 0; i < currentWord.length; i++){
-            printf("%c", currentWord.contents[i]);
-        }
-        if (currentChar == MARK){
-            printf("%c\n", MARK);
-        }
-        else{
-            printf("%c", BLANK);
-        }
-        advWord();
-    }
-    printf("YOI \n NEXT \n \n");
-    printf("Udah sampe sini\n");
+        fclose(tape);
+	}
+    // exit game
+	else{
+		printf("Exiting program...\nSee you later!");
+		exit(0);
+	}
 }
